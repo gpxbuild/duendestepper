addc3b6228e72ec8e01153e75d30aa23693f89be
Egbert <bouwhuis@gpx.nl>
2018-09-17 09:16:48 +0200

    add arduino sketch

--- duendestepper.ino
+++ duendestepper.ino
@@ -20,25 +20,27 @@
 
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-                                     // for your motor
+const int stepsPerRevolution = 4096 * 3;  // change this to fit the number of steps per revolution
+// for your motor
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8,9,10,11);            
-
-int stepCount = 0;         // number of steps the motor has taken
+Stepper myStepper(stepsPerRevolution, 3, 2);
 
 void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(60);
   // initialize the serial port:
   Serial.begin(9600);
 }
 
 void loop() {
-  // step one step:
-  myStepper.step(1);
-  Serial.print("steps:" );
-  Serial.println(stepCount);
-  stepCount++;
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
   delay(500);
 }
-
